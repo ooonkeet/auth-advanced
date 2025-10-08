2ee6383082c927562a193ae1a929611ad991bbab
Ankit <ankitspare981@gmail.com>
2025-10-08 15:18:02 +0530

    Connected mongoDB successfully with error handling.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express"
+import 'dotenv/config'
+import connectDB from "./database/db.js"
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT||3000
 app.listen(PORT,()=>{
+    connectDB()
     console.log(`Server is listening at port ${PORT}`);
 })
