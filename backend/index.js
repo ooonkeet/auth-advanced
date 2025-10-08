09ec2ff67b02e8ff7d26ef48b39db6de01b9bc45
Ankit <ankitspare981@gmail.com>
2025-10-08 16:45:47 +0530

    Route created for basic api testing (verified by postman) and a basic middleware token in form of app.use(express.json()) is used in the entry point of backend.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import 'dotenv/config'
 import connectDB from "./database/db.js"
+import userRoute from "../backend/routes/userRoute.js"
 const app = express()
 const PORT = process.env.PORT||3000
+app.use(express.json())
+app.use('/user',userRoute)
+// http://localhost:8000/user/register -> endpoint (post)
 app.listen(PORT,()=>{
     connectDB()
     console.log(`Server is listening at port ${PORT}`);
